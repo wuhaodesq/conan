3e144415b5c37d46da8705a04b653412f0c59558
wuhao <wuhaodes@qq.com>
2026-04-01 10:44:13 +0800

    Bootstrap project README and training pipeline scaffold

---
+++ src/hybrid_trainer/__init__.py
@@ -0,0 +1,17 @@
+"""Hybrid self-improvement training prototype."""
+
+from .pipeline import (
+    Decision,
+    DecisionNode,
+    IterationReport,
+    PipelineConfig,
+    TrainingPipeline,
+)
+
+__all__ = [
+    "Decision",
+    "DecisionNode",
+    "IterationReport",
+    "PipelineConfig",
+    "TrainingPipeline",
+]
