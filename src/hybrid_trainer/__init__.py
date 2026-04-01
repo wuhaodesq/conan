8216448285d8431f68188de1af63b918931c396e
wuhao <wuhaodes@qq.com>
2026-04-01 16:44:26 +0800

    Add strategy manager and automatic strategy switching

---
+++ src/hybrid_trainer/__init__.py
@@ -0,0 +1,40 @@
+"""Hybrid self-improvement training prototype."""
+
+from .engine import CycleResult, TrainingEngine
+from .evaluation import AutoEvaluator, EvaluationResult
+from .generation import TaskGenerator, TaskSample
+from .human_review import HumanReviewDecision, HumanReviewItem, HumanReviewQueue
+from .metrics import DecisionMetrics, summarize_decisions
+from .pipeline import (
+    Decision,
+    DecisionNode,
+    IterationReport,
+    PipelineConfig,
+    TrainingPipeline,
+)
+from .strategy import StrategyManager, StrategySwitchRecord, TrainingStrategy
+from .triggers import NodeTriggerRecommendation, recommend_major_nodes
+
+__all__ = [
+    "AutoEvaluator",
+    "CycleResult",
+    "Decision",
+    "DecisionMetrics",
+    "DecisionNode",
+    "EvaluationResult",
+    "HumanReviewDecision",
+    "HumanReviewItem",
+    "HumanReviewQueue",
+    "IterationReport",
+    "NodeTriggerRecommendation",
+    "PipelineConfig",
+    "StrategyManager",
+    "StrategySwitchRecord",
+    "TaskGenerator",
+    "TaskSample",
+    "TrainingEngine",
+    "TrainingPipeline",
+    "TrainingStrategy",
+    "recommend_major_nodes",
+    "summarize_decisions",
+]
